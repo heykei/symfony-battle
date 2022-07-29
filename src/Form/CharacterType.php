ef863a028fa3bdba2baf7febddddbd7e600aca21
heykei <trupin.kenny@gmail.com>
2022-07-29 10:38:37 +0200

    last_push

---
+++ src/Form/CharacterType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Character;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CharacterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('job')
+            ->add('life')
+            ->add('attack')
+            ->add('def')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Character::class,
+        ]);
+    }
+}
